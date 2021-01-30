257731f45faab9e6d4845e3c07365f2eb946e3bc
zsbai <62942942+zsbai@users.noreply.github.com>
2021-01-31 01:12:19 +0800

    Update rain.js

--- css/rain.js
+++ css/rain.js
@@ -1,64 +1,77 @@
-(function(){
-         var canvas = document.getElementByIdx_x('canvas');
-         var ctx = canvas.getContext('2d');
-         var w = document.documentElement.offsetWidth;
-         var h = document.documentElement.offsetHeight;
-         var x = 0, y = 0,len = 200,angle = -2,count = 100;
-         var rainTimer = null,drawTimer = null;
- 
-         //线条颜色
-         var color = ctx.createLinearGradient(0,0,0,len);
-         color.addColorStop(0,'purple');
-         color.addColorStop(1,'rgba(255,255,255,0.2)');
- 
-         //ctx.strokeStyle = 'rgba(255,255,255,0.2)';
-         ctx.strokeStyle = color;
- 
-         function drawRain(x,y)
-         {       
-               //每次绘制渐变线条 都需要找到坐标
-               var color = ctx.createLinearGradient(x,y,x+angle,y+len);
-               //color.addColorStop(0,'rgba(254,139,199,0.3)');
-               color.addColorStop(0,'rgba(0,0,0,0.1');
-               color.addColorStop(1,'rgba(255,255,255,0.2)');
-               ctx.strokeStyle = color;
- 
-               ctx.beginPath()
-               ctx.moveTo(x,y);
-               ctx.lineWidth=1;
-               ctx.lineTo(x + angle,y+len);
-               ctx.stroke();
- 
-         }
- 
- 
-         //绘制满屏的雨滴
-         function fullWindowRain()
-         {       
-              var i = 0;
-              for(i = 0;i < count; i++)
-              {
-                     drawRain(w*Math.random(),h*Math.random());
-              }
-         }
- 
-         //改变大雨或者小雨
-         function changeRain()
-         {
-              rainTimer = setInterval(function(){
-                     count  = Math.ceil(500 + 100 * Math.random());
-              },2000);
-         }
- 
-         changeRain();
-         reDraw();
- 
-         //重绘的频率
-         function reDraw()
-         {
-             drawTimer = setInterval(function(){
-                 ctx.clearRect(0,0,w,h+200);
-                 fullWindowRain();
-             },100);
-         }
-})();
+		var canvas = document.querySelector(".raining");//获取class为.raning的控件
+		var w, h;
+		~~ function setSize() { //定义canvas的宽高，让他跟浏览器的窗口的宽高相同
+			window.onresize = arguments.callee;
+			w = window.innerWidth;
+			h = window.innerHeight;
+			canvas.width = w;
+			canvas.height = h;
+		}();
+
+		var canCon = canvas.getContext("2d"); //建立2d画板
+		var arain = []; //新建数组,储存雨滴
+		//
+		function random(min, max) { //返回最小值到最大值之间的值
+			return Math.random() * (max - min) + min;
+		}
+
+		function rain() {};
+		rain.prototype = {
+			init: function() { //变量初始化
+				this.x = random(0, w); //在0-w之间生成雨滴
+				this.vx = 0.5;
+				this.y = 0;
+				this.vy = random(5, 10);
+				this.h = random(0.9 * h, h); //地板高度
+				this.r = 1; //雨滴绽放的半径
+				this.vr = 1;
+			},
+			draw: function() {
+				if(this.y < this.h) {
+					canCon.beginPath(); //拿起一支笔
+					canCon.fillStyle = "#CCC"; //笔墨的颜色
+					canCon.fillRect(this.x, this.y, 1, 10); //想象画一个雨滴
+				} else {
+					canCon.beginPath(); //拿起一支笔
+					canCon.strokeStyle = "#CCC"; //笔墨的颜色
+					canCon.arc(this.x, this.y, this.r, 0, Math.PI,1); //想象画一个半圆
+					canCon.stroke(); //下笔作画
+				}
+			},
+			move: function() {
+				if(this.y < this.h) { //位置判断，让雨滴下落
+					this.y += this.vy;
+					this.x += this.vx;
+				} else {
+					if(this.r < 30) { //落下后绽放的大小
+						this.r += this.vr;
+					} else {
+						this.init(); //放完后回归变量原点
+					}
+
+				}
+				this.draw(); //开始作画
+
+			}
+		}
+
+		function createrain(num) {
+			for(var i = 0; i < num; i++) {
+				setTimeout(function() {
+					var raing = new rain(); //创建一滴雨
+					raing.init();
+					raing.draw();
+					arain.push(raing);
+				}, 150 * i) //每隔150ms下落一滴雨
+			}
+		}
+		createrain(40); //雨滴数量
+		setInterval(function() {
+			canCon.fillStyle = "rgba(0,0,0,0.08)"; //拿起一支透明0.13的笔		
+			canCon.fillRect(0, 0, w, h); //添加蒙版 控制雨滴长度
+			for(var item of arain) {
+				//item of arain指的是数组里的每一个元素
+				//item in arain指的是数组里的每一个元素的下标（包括圆形连上的可遍历元素）
+				item.move(); //运行整个move事件
+			}
+		}, 500 / 60); //下落时间
